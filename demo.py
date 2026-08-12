fdcaf31a2d917ff6aa6fe0c4f2256c48f9ee3a3f
yash <yash@gmail.com>
2026-08-12 15:31:59 +0530

    Add dev branch files demo.py and sample.py

---
+++ demo.py
@@ -0,0 +1,5 @@
+def main():
+    print("Demo script running")
+
+if __name__ == '__main__':
+    main()
---
+++ sample.py
@@ -0,0 +1,5 @@
+def sample_function():
+    print("Sample function from dev branch")
+
+if __name__ == '__main__':
+    sample_function()
